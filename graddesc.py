724094e48b4e0a6ad33f5b6f2c7c6459cb872b26
sokuno222 <siena.okuno@students.olin.edu>
2017-12-04 15:14:38 -0500

    Implemented 3 dimensions. Started graphing

--- graddesc.py
+++ graddesc.py
@@ -1,11 +1,32 @@
-def find_min(learning, steps):
-    '''Finds the minimum of a given function using gradient descent'''
-    x = 10
-    function = x^2
-    for i in range(steps):
-        gradient = 2*x
-        x = x - learning*gradient
-        print(x)
-    return x
+import plotly.plotly as plotly
+import plotly.graph_objs as graphly
+import numpy as np
+def myFunc(x, y, z):
+    return (2*x, 2*y, 2*z)
 
-find_min(.1,100)
+def find_min(gradient, init, learning, steps):
+    '''Finds the minimum of a given function using gradient descent'''
+    x, y, z = init
+    coords = np.zeros((steps,3))
+    for i in range(steps):
+        dx, dy, dz = gradient(x, y, z)
+        x = x - dx*learning
+        y = y - dy*learning
+        z = z - dz*learning
+        coords[i,:] = [x,y,z]
+    return coords
+
+def graph_it(gradient, init, learning, steps):
+    data = find_min(myFunc, (10, 10, 10), .1, 3)
+    dadata = [graphly.Surface(data)]
+    layout = plotly.Layout(
+        title = 'Gradient Descent',
+        autosize = False,
+        width = 500,
+        height = 500,
+        margin = dict(l=65, r=50, b=65, t=90))
+    fig = plotly.Figure(data=data, layout=layout)
+    plotly.iplot(fig, filename = 'Gradient Descent Test')
+
+
+print(graph_it(myFunc, (10, 10, 10), .1, 3))
